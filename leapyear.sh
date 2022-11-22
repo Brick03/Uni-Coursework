3b5cca589977a7ab393e7c16af767450add378c8
Alistair McInnes <93935542+Brick03@users.noreply.github.com>
2022-11-22 17:24:18 -0500

    Add files via upload

--- leapyear.sh
+++ leapyear.sh
@@ -1,41 +1,34 @@
 #!/bin/bash
-year=$1
-number_of_args=$#
+year=$1    #take argurment for year 
+number_of_args=$# #takes number of args
 
-if ! [ $number_of_args == 1 ]
-	then
-		echo "Usage: leapyear.sh <year>"
-		exit 1
-		assert_success=false
-	else
-		assert_success=true
+if ! [ $number_of_args == 1 ] #checks number or args if not = to 1 it ends script
+    then
+    	echo "Usage: leapyear.sh <year>"
+	exit 1		
 fi
-if ! [[ "$year" =~ ^[0-9]+$ ]]
+if ! [[ "$year" =~ ^[0-9]+$ ]] #checks if input is integer if not ends script
     then
     	echo "Usage: leapyear.sh <year>"
         exit 1
-        assert_success=false
+fi
+if (( $year%4 == 0 )) #checks if year is divisable by 4 and if so sets check to true else sets to false
+    then
+        check=true
     else
-    	assert_success=true
+        check=false
 fi
-
-if (( $year%4 == 0 ))
+if (( $year%100 == 0 )) #checks if year is divisable by 100 and if so sets check to false
     then
-        c=true
+        check=false
+fi
+if (( $year%400 == 0 )) #checks if year is divisable by 400 and if so sets check to true
+    then
+        check=true
+fi
+if $check; #outputs result 
+    then
+	echo "true"
     else
-        c=false
-fi
-if (( $year%100 == 0 ))
-    then
-        c=false
-fi
-if (( $year%400 == 0 ))
-    then
-        c=true
-fi
-if $c; 
-	then
-		echo "true"
-	else
-		echo "false"
+	echo "false"
 fi
